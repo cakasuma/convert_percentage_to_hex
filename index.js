6ee5bd82744a480250a11e0bdfe72c6315ac8421
mustofa-binary <mustofa@regentmarkets.com>
2019-09-12 11:13:17 +0800

    convert percentage to hex

---
+++ index.js
@@ -0,0 +1,22 @@
+const standard_input = process.stdin;
+standard_input.setEncoding('utf-8');
+
+console.log('please enter percentage');
+
+standard_input.on('data', function (data) {
+
+    // User input exit.
+    if(data === 'exit\n'){
+        // Program exit.
+        console.log("User input complete, program exit.");
+        process.exit();
+    }else
+    {
+        const percentage = +data;
+        const decimal = percentage*255/100;
+        const hex_val = decimal.toString(16);
+
+        // Print user input in console.
+        console.log('Hex value is : ' + hex_val);
+    }
+});
